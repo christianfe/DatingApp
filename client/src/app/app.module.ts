13d7d51d07faaa694b6eee0534c71db01eea1133
christianfe <christian.felicione@gmail.com>
2023-01-04 15:43:18 +0100

    errors middleware, client and API  - S6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component'
 import { SharedModule } from './_modules/shared.module';
+import { TestErrorComponent } from './error/test-error/test-error.component';
 
 @NgModule({
 	declarations: [
@@ -24,7 +25,8 @@ import { SharedModule } from './_modules/shared.module';
 		MemberListComponent,
 		ListsComponent,
 		MessagesComponent,
-		MemberDetailComponent
+		MemberDetailComponent,
+  TestErrorComponent
 	],
 	imports: [
 		BrowserAnimationsModule,
