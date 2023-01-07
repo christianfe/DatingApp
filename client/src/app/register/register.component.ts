b797f81a04c4dc9458fc6ddc382f46f459440881
christianfe <christian.felicione@gmail.com>
2023-01-07 11:39:50 +0100

    added register form validations client

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -10,13 +11,34 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
 	@Output() cancelRegister = new EventEmitter();
 	model: any = {}
+	registerForm: FormGroup = new FormGroup({});
 
 	constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
 	ngOnInit(): void {
+		this.initializeForm();
+	}
+
+	initializeForm() {
+		this.registerForm = new FormGroup({
+			username: new FormControl('', [Validators.required]),
+			password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+			confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+		});
+		this.registerForm.controls['password'].valueChanges.subscribe({
+			next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+		})
+	}
+
+	matchValues(matchTo: string): ValidatorFn {
+		return (control: AbstractControl) => {
+			return control.value === control.parent?.get(matchTo)?.value ? null : { notMatching: true }
+		}
 	}
 
 	register() {
+		console.log(this.registerForm?.value)
+		/*
 		this.accountService.register(this.model).subscribe({
 			next: () => {
 				this.cancel();
@@ -24,7 +46,7 @@ export class RegisterComponent implements OnInit {
 			error: error => {
 				console.log(error);
 			}
-		})
+		})*/
 	}
 
 	cancel() {
