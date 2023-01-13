785c1ddae416bc8330073db2be26e2cb900bd5fd
christianfe <christian.felicione@gmail.com>
2023-01-13 14:23:55 +0100

    testng presence service..

---
+++ API/SignalR/PresenceHub.cs
@@ -0,0 +1,25 @@
+using API.Extensions;
+using CloudinaryDotNet;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.SignalR
+{
+
+	[Authorize]
+	public class PresenceHub : Hub
+	{
+		public PresenceHub() { }
+
+		public override async Task OnConnectedAsync()
+		{
+			await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+		}
+
+		public override async Task OnDisconnectedAsync(Exception? exception)
+		{
+			await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+			await base.OnDisconnectedAsync(exception);
+		}
+	}
+}
