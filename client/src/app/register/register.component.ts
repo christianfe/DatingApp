49628e4a532188db117c1ed65efc6715d6f8a83e
Christian Felicione <christian.felicione@gmail.com>
2023-04-08 00:41:44 +0200

    text added in sign up

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -5,67 +5,67 @@ import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
-	selector: 'app-register',
-	templateUrl: './register.component.html',
-	styleUrls: ['./register.component.css']
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-	@Output() cancelRegister = new EventEmitter();
-	registerForm: FormGroup = new FormGroup({});
-	maxDate: Date = new Date();
-	validationErrors: string[] | undefined
+  @Output() cancelRegister = new EventEmitter();
+  registerForm: FormGroup = new FormGroup({});
+  maxDate: Date = new Date();
+  validationErrors: string[] | undefined
 
-	constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder, private router: Router) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder, private router: Router) { }
 
-	ngOnInit(): void {
-		this.initializeForm();
-		this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
-	}
+  ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
 
-	initializeForm() {
-		this.registerForm = this.fb.group({
-			gender: ['male'],
-			username: ['', [Validators.required]],
-			knownAs: ['', [Validators.required]],
-			dateOfBirth: ['', [Validators.required]],
-			city: ['', [Validators.required]],
-			country: ['', [Validators.required]],
-			password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
-			confirmPassword: ['', [Validators.required, this.matchValues('password')]]
-		});
-		this.registerForm.controls['password'].valueChanges.subscribe({
-			next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
-		})
-	}
+  initializeForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', [Validators.required]],
+      knownAs: ['', [Validators.required]],
+      dateOfBirth: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      country: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
 
-	matchValues(matchTo: string): ValidatorFn {
-		return (control: AbstractControl) => {
-			return control.value === control.parent?.get(matchTo)?.value ? null : { notMatching: true }
-		}
-	}
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : { notMatching: true }
+    }
+  }
 
-	register() {
-		const dob = this.getDateOnly(this.registerForm.controls['dateOfBirth'].value);
-		const values = { ...this.registerForm.value, dateOfBirth: dob };
-		this.accountService.register(values).subscribe({
-			next: () => {
-				this.router.navigateByUrl('/members');
-			},
-			error: error => {
-				console.log(error);
-				this.validationErrors = error
-			}
-		})
-	}
+  register() {
+    const dob = this.getDateOnly(this.registerForm.controls['dateOfBirth'].value);
+    const values = { ...this.registerForm.value, dateOfBirth: dob };
+    this.accountService.register(values).subscribe({
+      next: () => {
+        this.router.navigateByUrl('/members');
+      },
+      error: error => {
+        console.log(error);
+        this.validationErrors = error
+      }
+    })
+  }
 
-	cancel() {
-		this.cancelRegister.emit(false);
-	}
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
 
 
-	private getDateOnly(dob: string | undefined) {
-		if (!dob) return;
-		let theDob = new Date(dob);
-		return new Date(theDob.setMinutes(theDob.getMinutes() - theDob.getTimezoneOffset())).toISOString().slice(0, 10);
-	}
+  private getDateOnly(dob: string | undefined) {
+    if (!dob) return;
+    let theDob = new Date(dob);
+    return new Date(theDob.setMinutes(theDob.getMinutes() - theDob.getTimezoneOffset())).toISOString().slice(0, 10);
+  }
 }
